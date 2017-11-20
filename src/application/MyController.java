d3cf81352bdf162fad731e0e81e62dd613943493
ASUS <huangqiang3@asiainfo.com>
2017-11-20 11:42:30 +0800

    creat

---
+++ src/application/MyController.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/** 
+* @author Soy 
+* @date 2017年11月20日 上午11:18:55 
+* @version 1.0 
+*/
+public class MyController implements Initializable {
+
+   @FXML
+   private Button myButton;
+
+   @FXML
+   private TextField myTextField;
+
+   @Override
+   public void initialize(URL location, ResourceBundle resources) {
+
+       // TODO (don't really need to do anything here).
+
+   }
+
+   // When user click on myButton
+   // this method will be called.
+   public void showDateTime(ActionEvent event) {
+       System.out.println("Button Clicked!");
+
+       Date now= new Date();
+
+       DateFormat df = new SimpleDateFormat("yyyy-dd-MM HH:mm:ss");
+       String dateTimeString = df.format(now);
+        // Show in VIEW
+        myTextField.setText(dateTimeString);
+
+   }
+
+}
